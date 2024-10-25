2bbe0607eb1bb142c7ca6f404e3f6c4b237be0f3
roshira <155329753+Roshira@users.noreply.github.com>
2024-10-25 14:53:15 +0300

    first cimmit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SiteRBC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
