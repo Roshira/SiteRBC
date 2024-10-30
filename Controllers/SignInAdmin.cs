b2274ff3717829ff45d1980a7bf315496386d6e6
roshira <155329753+Roshira@users.noreply.github.com>
2024-10-30 14:50:09 +0200

    Updaye frontend for admin

---
+++ Controllers/SignInAdmin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiteRBC.Controllers
+{
+	public class SignInAdmin : Controller
+	{
+		public IActionResult SignInAdm()
+		{
+			return View();
+		}
+	}
+}
