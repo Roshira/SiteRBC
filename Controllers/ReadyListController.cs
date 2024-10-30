62a001907c9d3c93fbd39aceca37bd3988f20d0a
roshira <155329753+Roshira@users.noreply.github.com>
2024-10-30 13:23:51 +0200

    Add ReadyListContoller and view Product

---
+++ Controllers/ReadyListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiteRBC.Controllers
+{
+	public class ReadyListController : Controller
+	{
+		public IActionResult Product()
+		{
+			return View();
+		}
+	}
+}
