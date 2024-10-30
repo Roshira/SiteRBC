fb1a36a29031298a766fb3c05ad4e1cfd905a803
roshira <155329753+Roshira@users.noreply.github.com>
2024-10-30 14:20:13 +0200

    Add sing in and img ready shelter

---
+++ Controllers/SignInUsers.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiteRBC.Controllers
+{
+	public class SignInUsers : Controller
+	{
+		public IActionResult SignIn()
+		{
+			return View();
+		}
+	}
+}
