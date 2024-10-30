dbe3a6a50ccc50ff32721f1dbc0b5b5ad8db6349
roshira <155329753+Roshira@users.noreply.github.com>
2024-10-30 14:35:55 +0200

    Add sing in admin

---
+++ Controllers/SingInAdmin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiteRBC.Controllers
+{
+    public class SingInAdmin : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
