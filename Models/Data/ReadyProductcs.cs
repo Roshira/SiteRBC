7d24993c3aa85985ada9e06de8823d574378b6e4
roshira <155329753+Roshira@users.noreply.github.com>
2024-11-01 14:36:13 +0200

    Update ReadyProduct

--- Models/Data/ReadyProductcs.cs
+++ Models/Data/ReadyProductcs.cs
@@ -2,5 +2,14 @@
 {
 	public class ReadyProductcs
 	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public decimal Price { get; set; }
+		public int height { get; set; }
+		public int width { get; set; }
+		public int length { get; set; }
+		public int timeForBuild { get; set; }
+
+
 	}
 }
