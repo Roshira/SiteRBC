10f0ab04b0ffac36c3cee899bd127e8dc680ab0a
roshira <155329753+Roshira@users.noreply.github.com>
2024-11-01 16:55:01 +0200

    Started creating AdminMenu

---
+++ Controllers/Admin/AdminFunctionalController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiteRBC.Controllers.Admin
+{
+	public class SignInAdminController : Controller
+	{
+		[HttpGet]
+		public IActionResult SignInAdmin()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult SignInAdmin(string email, string password)
+		{
+			if (email == "roshira@ukr.net" && password == "123qwe123qwe")
+			{
+				return RedirectToAction("AdminMenu", "SignInAdmin");
+			}
+			ViewBag.ErrorMessage = "Error with password";
+			return View();
+		}
+		public IActionResult AdminMenu()
+		{
+			return View();
+		}
+
+	}
+}
