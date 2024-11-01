1f87d905920495e4e2e02e3119a3c76522c10bc3
roshira <155329753+Roshira@users.noreply.github.com>
2024-11-01 15:14:26 +0200

    Already created database

---
+++ Models/ReadyProductContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SiteRBC.Models.Data;
+
+public class ReadyProductContext : DbContext
+{
+	public ReadyProductContext(DbContextOptions<ReadyProductContext> options)
+	: base(options)
+	{
+	}
+
+	public DbSet<ReadyProductcs> Products { get; set; }
+}
