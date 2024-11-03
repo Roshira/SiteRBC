1fe90f16466ed2061b44369eebbbc71b97cd7286
roshira <155329753+Roshira@users.noreply.github.com>
2024-11-03 20:52:26 +0200

    Add inf in data base

---
+++ Controllers/AddReadyProducts/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SiteRBC.Models.Data;
+
+namespace SiteRBC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ReadyProductContext _context;
+
+        public ProductsController(ReadyProductContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ReadyProductcs product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("AdminMenu", "SignInAdmin");
+            }
+            return View(product);
+        }
+    }
+}
--- Models/Data/ReadyProductcs.cs
+++ Models/Data/ReadyProductcs.cs
@@ -1,14 +1,23 @@
-﻿namespace SiteRBC.Models.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SiteRBC.Models.Data
 {
 	public class ReadyProductcs
 	{
 		public int Id { get; set; }
-		public string Name { get; set; }
-		public decimal Price { get; set; }
-		public int height { get; set; }
-		public int width { get; set; }
-		public int length { get; set; }
-		public int timeForBuild { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(0.01, double.MaxValue, ErrorMessage = "Writing price pls")]
+        public decimal Price { get; set; }
+        [Range(0, int.MaxValue)]
+        public int height { get; set; }
+        [Range(0, int.MaxValue)]
+        public int width { get; set; }
+        [Range(0, int.MaxValue)]
+        public int length { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int timeForBuild { get; set; }
 
 
 	}
