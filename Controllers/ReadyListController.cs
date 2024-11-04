fd32d198fb54d0155969ffae56ce670b43f2d7a0
roshira <155329753+Roshira@users.noreply.github.com>
2024-11-04 19:50:09 +0200

    Create print data with database on Product page

--- Controllers/ReadyListController.cs
+++ Controllers/ReadyListController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using SiteRBC.Models.Data;
 
 namespace SiteRBC.Controllers
 {
 	public class ReadyListController : Controller
-	{
-		public IActionResult Product()
-		{
-			return View();
-		}
-	}
+    {
+        private readonly ReadyProductContext _context;
+
+        public ReadyListController(ReadyProductContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Product()
+        {
+            //Loading data with database
+            List<ReadyProductcs> products = await _context.Products.ToListAsync();
+            return View(products);
+        }
+    }
 }
