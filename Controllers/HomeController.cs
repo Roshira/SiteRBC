af55bbebcd3e67a94b99ba277e6272128c398063
roshira <155329753+Roshira@users.noreply.github.com>
2024-11-09 20:33:17 +0200

    Add controller AboutUs and Fouder

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace SiteRBC.Controllers
             return View();
         }
 
-        public IActionResult AboutUs()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- Models/Data/CallСustomerBack.cs
+++ Models/Data/CallСustomerBack.cs
@@ -1,6 +1,14 @@
-﻿namespace SiteRBC.Models.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SiteRBC.Models.Data
 {
     public class CallСustomerBack
     {
+        public int Id { get; set; }
+        [Required]
+        public string Number { get; set; }
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
     }
 }
